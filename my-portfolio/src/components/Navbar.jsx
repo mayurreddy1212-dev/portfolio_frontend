0e06ede6ccfcbc3632f6e5caede305b0ed383ece
Mayur <mayurreddy1212@gmail.com>
2026-02-06 23:17:31 +0530

    vercel dependancy final

--- my-portfolio/src/components/Navbar.jsx
+++ my-portfolio/src/components/Navbar.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import logo from "./assets/logo.png";
+import logo from "../assets/logo.png";
 
 const Navbar = () => {
   const [open, setOpen] = useState(false);
